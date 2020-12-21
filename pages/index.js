3261657d5e2f5170409ae675645a338b0b0bc10f
kevin <krecoding@gmail.com>
2020-12-21 15:47:49 -0500

    login/out

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,44 @@
+import React, { useEffect, useState } from "react";
+import netlifyAuth from "../lib/netlifyAuth";
+
 const IndexPage = () => {
+	let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated);
+	let [user, setUser] = useState(null);
+
+	useEffect(() => {
+		netlifyAuth.initialize((user) => {
+			setLoggedIn(!!user);
+			setUser(user);
+		});
+	}, [loggedIn]);
+
+	let login = () => {
+		netlifyAuth.authenticate((user) => {
+			setLoggedIn(!!user);
+			setUser(user);
+			netlifyAuth.closeModal();
+		});
+	};
+
+	let logout = () => {
+		netlifyAuth.signout(() => {
+			setLoggedIn(false);
+			setUser(null);
+		});
+	};
+
 	return (
-		<div>
-			<h1>hello world!</h1>
-		</div>
+		<React.Fragment>
+			{loggedIn ? (
+				<div>
+					You are logged in!
+					<br />
+					<button onClick={logout}>Log out here.</button>
+				</div>
+			) : (
+				<button onClick={login}>Log in here.</button>
+			)}
+		</React.Fragment>
 	);
 };
 
