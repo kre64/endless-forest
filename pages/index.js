bbe431587d10ad41dc50cb17f8c10bdc10711488
kevin <krecoding@gmail.com>
2020-12-21 15:25:48 -0500

    new route

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,7 @@
-// pages/index.js
-
-import PlayersList, {
-	ALL_CLUBS_QUERY,
-	ALL_COUNTRIES_QUERY,
-} from "components/PlayersList";
-import Link from "next/link";
-
 const IndexPage = () => {
 	return (
 		<div>
-			<h1 style={{ textAlign: "center" }}>EPL Players Directory</h1>
-			<PlayersList />
+			<h1>hello world!</h1>
 		</div>
 	);
 };
---
+++ pages/worlds.js
@@ -0,0 +1,18 @@
+// pages/index.js
+
+import PlayersList, {
+	ALL_CLUBS_QUERY,
+	ALL_COUNTRIES_QUERY,
+} from "components/PlayersList";
+import Link from "next/link";
+
+const Worlds = () => {
+	return (
+		<div>
+			<h1 style={{ textAlign: "center" }}>EPL Players Directory</h1>
+			<PlayersList />
+		</div>
+	);
+};
+
+export default Worlds;
