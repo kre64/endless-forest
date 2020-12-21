da842124908086bf1462436eb69c133647875092
kevin <krecoding@gmail.com>
2020-12-20 19:58:27 -0500

    .

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 module.exports = {
+  target: 'serverless',
   env: {
     // Reference a variable that was defined in the .env file and make it available at Build Time
     SLASH_ENDPOINT: process.env.SLASH_ENDPOINT,
