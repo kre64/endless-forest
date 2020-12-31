6bb5002e74ef85eee07f67abbc6ead7b2d07cdfb
kevin <krecoding@gmail.com>
2020-12-30 21:53:34 -0500

    testing GQL + gotruejs custom userdata

--- lambdas/identity-signup.js
+++ lambdas/identity-signup.js
@@ -1,31 +1,36 @@
+import axios from "axios";
 import { gql, useQuery } from "@apollo/client";
 
 const { identity, user } = context.clientContext;
 
-// const INSERT_USER = gql`
-// mutation insert_single_user {
-// 	insert_user(
-// 		object: {
-// 			level: 0,
-// 			items: {}
-// 			xp: 0,
-// 			name: ${user.app_metadata.name}
-// 		}
-// 	)
-// }
-// `;
+const INSERT_USER = gql`
+	mutation insert_single_users {
+		insert_users_one(
+			object: { name: ${user.user_metadata.name}, level: 0, inventory: "1, 2, 3", xp: 0 }
+		) {
+			name
+			level
+			inventory
+			xp
+		}
+	}
+`;
 
 exports.handler = async function (event, context) {
-	// useQuery(INSERT_USER);
+	useQuery(INSERT_USER);
+
+	const res = await axios.put("/user", {
+		data: {
+			level: 0,
+			inventory: "",
+			xp: 0
+		},
+	});
 
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
-			user_metadata: {
-				level: 0,
-				items: {},
-				xp: 0,
-			},
+			app_metadata: ["adventurer"],
 		}),
 	};
 };
