4065784f2f86bbd94ab591399748600a1c5bb19e
kevin <krecoding@gmail.com>
2020-12-30 21:10:31 -0500

    testing gql insert at signup

--- lambdas/identity-signup.js
+++ lambdas/identity-signup.js
@@ -1,7 +1,31 @@
+import { gql, useQuery } from "@apollo/client";
+
+const { identity, user } = context.clientContext;
+
+const INSERT_USER = gql`
+mutation insert_single_user {
+	insert_user(
+		object: {
+			level: 0,
+			items: {}
+			xp: 0,
+			name: ${user.app_metadata.name}
+		}
+	)
+}
+`;
+
 exports.handler = async function (event, context) {
-	const { identity, user } = context.clientContext;
+	useQuery(ALL_WORLDS_QUERY);
+
 	return {
 		statusCode: 200,
-		body: JSON.stringify({ app_metadata: { roles: ["member"] } }),
+		body: JSON.stringify({
+			app_metadata: {
+				level: 0,
+				items: {},
+				xp: 0,
+			},
+		}),
 	};
 };
