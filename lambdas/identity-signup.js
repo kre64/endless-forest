6ee2cc867fa6ad09941c4b779ab5441ff0b7f0f9
kevin <krecoding@gmail.com>
2020-12-30 22:41:51 -0500

    new library for node

--- lambdas/identity-signup.js
+++ lambdas/identity-signup.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const { gql, useQuery } = require("@apollo/client");
+import { request, gql } from "graphql-request";
 
 exports.handler = async function (event, context) {
 	const INSERT_USER = gql`
@@ -14,7 +14,13 @@ exports.handler = async function (event, context) {
 		}
 	`;
 
-	useQuery(INSERT_USER);
+	request(process.env.GQL_ENDPOINT, query).then((data) => console.log(data));
+
+	const client = new GraphQLClient(process.env.GQL_ENDPOINT, {
+		headers: { "x-hasura-admin-secret": process.env.GQL_AUTH_TOKEN },
+	});
+
+	client.request(INSERT_USER).then((data) => console.log(data));
 
 	const res = await axios.put("/user", {
 		data: {
