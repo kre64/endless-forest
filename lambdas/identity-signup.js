3e9f4470149761cbc2a6d4bbae582821aacdfd64
kevin <krecoding@gmail.com>
2020-12-30 21:26:06 -0500

    user metadata

--- lambdas/identity-signup.js
+++ lambdas/identity-signup.js
@@ -21,7 +21,7 @@ exports.handler = async function (event, context) {
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
-			app_metadata: {
+			user_metadata: {
 				level: 0,
 				items: {},
 				xp: 0,
