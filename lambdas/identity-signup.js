976d9eb46b10d0a49ba65589cc5f141ec1c2a5c0
kevin <krecoding@gmail.com>
2020-12-30 20:47:25 -0500

    lambda testing

--- lambdas/identity-signup.js
+++ lambdas/identity-signup.js
@@ -2,6 +2,6 @@ exports.handler = async function (event, context) {
 	const { identity, user } = context.clientContext;
 	return {
 		statusCode: 200,
-		body: "hello!"
+		body: JSON.stringify({ app_metadata: { roles: ["member"] } }),
 	};
 };
